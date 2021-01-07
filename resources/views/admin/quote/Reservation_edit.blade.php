9a02477c385a7e9472586843a0552168925ceccd
freejob985 <freejob985@gmail.com>
2021-01-07 16:52:33 +0200

    asd

--- resources/views/admin/quote/Reservation_edit.blade.php
+++ resources/views/admin/quote/Reservation_edit.blade.php
@@ -133,7 +133,7 @@
       <div class="card-footer">
         <div class="row">
           <div class="d-inline-block mx-auto">
-            {{$quotes->links()}}
+          
           </div>
         </div>
       </div>
@@ -142,43 +142,5 @@
 </div>
 
 
-<!-- Send Mail Modal -->
-<div class="modal fade" id="mailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
-  aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Send Mail</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form id="ajaxEditForm" class="" action="{{route('admin.quotes.mail')}}" method="POST">
-          @csrf
-          <div class="form-group">
-            <label for="">Client Mail **</label>
-            <input id="inemail" type="text" class="form-control" name="email" value="" placeholder="Enter email">
-            <p id="eerremail" class="mb-0 text-danger em"></p>
-          </div>
-          <div class="form-group">
-            <label for="">Subject **</label>
-            <input id="insubject" type="text" class="form-control" name="subject" value="" placeholder="Enter subject">
-            <p id="eerrsubject" class="mb-0 text-danger em"></p>
-          </div>
-          <div class="form-group">
-            <label for="">Message **</label>
-            <textarea id="inmessage" class="form-control summernote" name="message" data-height="150"
-              placeholder="Enter message"></textarea>
-            <p id="eerrmessage" class="mb-0 text-danger em"></p>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button id="updateBtn" type="button" class="btn btn-primary">Send Mail</button>
-      </div>
-    </div>
-  </div>
-</div>
+
 @endsection
