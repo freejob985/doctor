eccba515cc5fac1dc60c29999cf520b553f7b8c3
freejob985 <freejob985@gmail.com>
2021-01-07 22:36:19 +0200

    شسي

--- resources/views/admin/quote/quote-details.blade.php
+++ resources/views/admin/quote/quote-details.blade.php
@@ -56,6 +56,7 @@
             </div>
             <div class="col-lg-8">
               {{convertUtf8($quote->generateRandomString)}}
+              
             </div>
           </div>
           <hr>
