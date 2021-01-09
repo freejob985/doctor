a38e08cf0710df82d3b4b7c56d385995c69eba2b
freejob985 <freejob985@gmail.com>
2021-01-09 19:11:19 +0200

    asd

--- resources/views/remind.blade.php
+++ resources/views/remind.blade.php
@@ -21,14 +21,53 @@
     </style>
 
 </head>
-
 <body style="margin: 0; padding: 0;">
-    <P>
-        <p>{!!$array['Noun']!!}</p>
-        <P> we reminder you that you have an appointment with Dr. med. Dieter Roettger for {!!$array['Detection_type']!!}</P>
-        <P>{!!$array['History']!!}</P>
-        <P>Regards</P>
-    </P>
-</body>
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+            <td style="padding: 20px 0 30px 0;">
 
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="600"
+                    style="border-collapse: collapse; border: 1px solid #cccccc;">
+                    <tr>
+                        <td align="center" bgcolor="#70bbd9" style="padding: 40px 0 30px 0;">
+                            <img src="https://sub.alriyadah-tr.com/assets/front/img/5ff4f902171b8.png" alt="Creating Email Magic." width="300"
+                                height="230" style="display: block;" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%"
+                                style="border-collapse: collapse;">
+
+
+
+
+
+                                <tr>
+                                    <td style="color: #153643; font-family: Arial, sans-serif;">
+                                        <P>
+                                            <p>{!!$array['Noun']!!}</p>
+                                            <P> we reminder you that you have an appointment with Dr. med. Dieter Roettger for {!!$array['Detection_type']!!}</P>
+                                            <P>{!!$array['History']!!}</P>
+                                            <P>Regards</P>
+                                        </P>
+                                    </td>
+                                </tr>
+                               
+
+
+                    
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+
+                    </tr>
+                </table>
+
+            </td>
+        </tr>
+    </table>
+</body>
+@dd("Catch errors for script and full tracking ( 6)");
 </html>
