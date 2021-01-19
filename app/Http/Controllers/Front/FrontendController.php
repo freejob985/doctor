8eac59d07c945c6046ef0fe286ac92afdc571783
freejob985 <freejob985@gmail.com>
2021-01-19 13:23:47 +0200

    asd

--- app/Http/Controllers/Front/FrontendController.php
+++ app/Http/Controllers/Front/FrontendController.php
@@ -666,7 +666,7 @@ class FrontendController extends Controller
     {
         // dd($request->all());
 
-        $reservation = DB::table('Reservation')->get();
+        $reservation = DB::table('Reservation')->where('data', $request->input('data'))->get();
         dd($reservation);
 
     }
