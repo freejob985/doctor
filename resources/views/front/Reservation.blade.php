6935b6b8f7bfab7800f778835e31e78743543e52
freejob985 <freejob985@gmail.com>
2021-01-20 17:36:16 +0200

    asd

--- resources/views/front/Reservation.blade.php
+++ resources/views/front/Reservation.blade.php
@@ -1,3 +1,7 @@
+@php
+$Dates = DB::table('Dates')->value('Dates');
+@endphp
+
 @extends("front.$version.layout")
 
 @section('pagename')
@@ -9,41 +13,57 @@
 <style>
   .img-responsive {
     margin: 0 auto !important;
-}
-{{--  div#img {
-  margin-top: -24%;
-}  --}}
+  }
+
+    {
+      {
+      -- div#img {
+        margin-top: -24%;
+      }
+
+      --
+    }
+  }
 
 
 
-.ui-datepicker {
+  .ui-datepicker {
     width: 100%;
     padding: 1.2em 1.2em 0;
     display: none;
-}
+  }
 
 
-.ui-datepicker .ui-datepicker-header {
+  .ui-datepicker .ui-datepicker-header {
     position: relative;
     padding: .2em 0;
     background: #3397C8;
-}.ui-datepicker th {
+  }
+
+  .ui-datepicker th {
     padding: 12px;
     color: #3397C8;
     text-align: center;
     font-weight: bold;
     border: 0;
-}.ui-state-default, .ui-widget-content .ui-state-default, .ui-widget-header .ui-state-default, .ui-button, html .ui-button.ui-state-disabled:hover, html .ui-button.ui-state-disabled:active {
+  }
+
+  .ui-state-default,
+  .ui-widget-content .ui-state-default,
+  .ui-widget-header .ui-state-default,
+  .ui-button,
+  html .ui-button.ui-state-disabled:hover,
+  html .ui-button.ui-state-disabled:active {
     border: 1px solid #c5c5c5;
     background: #f6f6f6;
     font-weight: normal;
     font-size: 19px;
     color: #3397C8;
-    
-}
+
+  }
 </style>
 @section('content')
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <!--   breadcrumb area start   -->
@@ -71,40 +91,38 @@
   @csrf
   <div class="quote-area">
     <div class="container">
+      <p>
+        {!! $Dates !!}
+      </p>
       <div class="form-row">
-             <div class="col-md-2"></div>
+        <div class="col-md-2"></div>
         <div class="col-md-8 mb-4">
-<div id="editor">
-    asdasd
 
-asd
-
-
-
-</div>
 
           <input style="
     display: none;
-" style="background:blue;padding:33px;" type="date" placeholder="Please enter data" class="form-control day" dateformat="d M y" name="day"
-            id="date_input"    min="1997-01-01" max="2030-12-31">
- <div id="datepicker"></div>
+" style="background:blue;padding:33px;" type="date" placeholder="Please enter data" class="form-control day"
+            dateformat="d M y" name="day" id="date_input" min="1997-01-01" max="2030-12-31">
+          <div id="datepicker"></div>
 
-              @if ($errors->has('day'))
+          @if ($errors->has('day'))
           <div class="invalid-feedback">
             {{ $errors->first('day') }}
           </div>
           @endif
         </div>
       </div>
-      
+
     </div>
   </div>
 </form>
 <div id="img" class="text-center" style="display: none">
-    <div class="container">
-         <div class="row">
-  <img class="img-responsive center-block" style="width:200px;" src="https://i.pinimg.com/originals/78/e8/26/78e826ca1b9351214dfdd5e47f7e2024.gif" alt="Chania">
-</div></div>
+  <div class="container">
+    <div class="row">
+      <img class="img-responsive center-block" style="width:200px;"
+        src="https://i.pinimg.com/originals/78/e8/26/78e826ca1b9351214dfdd5e47f7e2024.gif" alt="Chania">
+    </div>
+  </div>
 </div>
 <!--   quote area start   -->
 <div class="quote-area">
@@ -117,7 +135,7 @@ asd
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 <script>
   $(document).ready(function(){
@@ -127,7 +145,7 @@ asd
           });
         });
 </script>
-  <script>
+<script>
   $( function() {
     $( "#datepicker" ).datepicker({
    onSelect: function(dateText, inst) {
@@ -153,13 +171,13 @@ asd
 });
     
   } );
-  </script>
-  
-  
+</script>
+
+
 <script src="https://cdn.ckeditor.com/ckeditor5/24.0.0/classic/ckeditor.js"></script>
 
-   <script>
-    ClassicEditor
+<script>
+  ClassicEditor
         .create( document.querySelector( '#editor' ) )
         .catch( error => {
             console.error( error );
